610ffcbfba984f335571e9295b1b9071c279bbb5
Andrei M <andreimartinenco.dev@gmail.com>
2021-07-14 02:35:47 -0400

    implemented the code that pulls in shop data from firestore and stores it into the shop reducer. Also included withSpinner HOC.

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import { CustomButtonContainer } from './custom-button.styles';
+import './custom-button.styles.scss';
 
 const CustomButton = ({children, ...props}) => (
-    <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
+    <CustomButtonContainer className='custom-button' {...props}>{children}</CustomButtonContainer>
 );
 
 export default CustomButton;
