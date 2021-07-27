0062e313ca568ef6a0031d21b1d972f70f09e4aa
Andrei M <andreimartinenco.dev@gmail.com>
2021-07-27 03:28:46 -0400

    Heroku redeploy

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -6,7 +6,7 @@ const StripeCheckoutButton = ({ price }) => {
 
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51JBX4xKv579Enm16d5bGcYpBNnvMemvyzN3k3v4ZVBNnVHS6EsRLo5Mj9Zh68nCWxZ6Y9xITFdih3R3dBzH6Hkwg00rqbPMMNB';
-
+ 
     const onToken = token => {
         axios({
             url: 'payment', 
